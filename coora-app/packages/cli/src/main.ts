dd3ae0e5c366fa6f33e5b310e7f25cf479ff980b
Peter Hayman <peteyhayman@gmail.com>
2022-11-14 17:15:24 +1100

    patch

--- coora-app/packages/cli/src/main.ts
+++ coora-app/packages/cli/src/main.ts
@@ -1,4 +1,13 @@
-
-
+//until stackblitz supports node 17
+// import fetch from 'node-fetch'
+export { }
 
 console.log('howdy!')
+async function main(){
+
+	const foo = await fetch('http://www.google.com')
+	console.dir(foo)
+
+
+}
+main()
