035467d9865dfd2290e9850d0d3c7fbfc0e5ed14
Peter Hayman <peteyhayman@gmail.com>
2022-11-15 14:19:05 +1100

    prepare form mdns

--- targets/coora_target_esp32/examples/mdns_server.rs
+++ targets/coora_target_esp32/examples/mdns_server.rs
@@ -1,9 +1,12 @@
 use anyhow::Result;
 use coora_target_esp32::*;
+//not yet released...
+// use esp_idf_svc::mdns;
 
 fn main() -> Result<()> {
     let wifi = wifi::Connection::new(secret::SSID, secret::PASSWORD)?;
     let _server = wifi.start_server()?;
+    // mdns::
     println!("howdy!");
     utility::sleep_forever();
 }
